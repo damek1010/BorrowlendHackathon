3508db3f7687aa19375d354ed5d8f97f62e3fe70
= <jdequo@gmail.com>
2018-04-21 12:40:57 +0200

    Added configuration file

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'displayErrorDetails' => true,
+    'addContentLengthHeader' => false,
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'password' => '',
+        'dbname' => 'borrowlend'
+    ]
+];
--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
-echo "a";
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$config = require ('config.php');
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $conn = new mysqli('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'], $db['user'], $db['pass']);
+    return $conn;
+};
+$app = new \Slim\App(['settings' => $config]);
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
